cf07ba07edf188e2d8a3944c11d22c7d0c6b0824
Vital Nahorny <vital.nagorny@gmail.com>
2021-02-03 15:04:15 +0300

    refactor: add files project

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import burgerMenuInit from './burger';
-import App from './App';
-
-window.addEventListener('DOMContentLoaded', () => {
-  burgerMenuInit();
-  const game = new App();
-  game.init().generateMainPage();
-  game.loadStatistic();
-});
